672eb6f1db741c981ee1611eac8f4aa670d727d9
zhangzhenkang <289259688@qq.com>
2016-03-09 11:22:11 +0800

    something new

---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int sun,moon,star;
+	printf("This is a C program.\n");
+	printf("Input the Sun,Moon,Star:\n");
+	sun  = getchar();
+	moon = getchar();
+	star = getchar();
+}
